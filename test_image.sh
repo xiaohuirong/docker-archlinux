cee11f8df822b46674dbc62cfd200d0d498d0b77
hoream <hoream@qq.com>
2025-10-07 23:58:56 +0800

    prepare add podman test

--- test_image.sh
+++ test_image.sh
@@ -16,3 +16,16 @@ docker run --rm -it \
     -e USER_ID=$(id -u) \
     -e GROUP_ID=$(id -g) \
     $IMAGE_NAME
+
+# --userns=keep-id: map host user uid to guest same uid
+# without this will map to guest 0(root)
+
+# podman run --rm -it \
+#     --gpus all \
+#     --ipc=host \
+#     -p 2022:22 \
+#     -v $HOME:/workspace \
+#     -e USER_ID=$(id -u) \
+#     -e GROUP_ID=$(id -g) \
+#     --userns=keep-id \
+#     $IMAGE_NAME
